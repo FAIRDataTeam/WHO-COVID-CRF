25418d72f88584aa655266168fd8597026f93e0f
NuriaQueralt <nqueralt.r@gmail.com>
2020-09-16 08:01:35 +0200

    updated form2triples.py python 3.8

--- Script/synth-sampler/synth_sampler.py
+++ Script/synth-sampler/synth_sampler.py
@@ -5,44 +5,50 @@
 # @author: Núria Queralt Rosinach
 # @email: n.queralt_rosinach@lumc.nl
 
-# This script is based on the WHO CODID-19 CRF RDF-wizard './form2triples-py3.8/form2triples_py3.8.py' adapted to python 3.8.
+# This script is based on the WHO CODID-19 CRF RDF-wizard './form2triples-py3.8/form2triples_py3.8.py'
+# adapted to python 3.8.
 
 # TODO: code to generate synthetic fair data
 """Script to generate synthetic fair data"""
 
-import rdflib, csv
 from datetime import datetime
-from rdflib import *
-from os import listdir
+import os
+import csv
+import rdflib
 
+
+# variables
 var2class = {}
 readfiles = []
 onto = rdflib.Graph()
 g = rdflib.Graph()
-n = Namespace('http://purl.org/vodan/whocovid19crfsemdatamodel/')
-DC = Namespace('http://purl.org/dc/elements/1.1/')
-part_of = URIRef('http://purl.obolibrary.org/obo/BFO_0000050')
-yes = URIRef('http://purl.org/vodan/whocovid19crfsemdatamodel/instances/C49488')
-no = URIRef('http://purl.org/vodan/whocovid19crfsemdatamodel/instances/C49487')
-unknown = URIRef('http://purl.org/vodan/whocovid19crfsemdatamodel/instances/C17998')
-not_done = URIRef('http://purl.org/vodan/whocovid19crfsemdatamodel/instances/Not_done')
-negative = URIRef('http://purl.org/vodan/whocovid19crfsemdatamodel/instances/Negative')
-positive = URIRef('http://purl.org/vodan/whocovid19crfsemdatamodel/instances/Positive')
-unit_label = URIRef('http://purl.obolibrary.org/obo/IAO_0000039')
+n = rdflib.Namespace('http://purl.org/vodan/whocovid19crfsemdatamodel/')
+DC = rdflib.Namespace('http://purl.org/dc/elements/1.1/')
+part_of = rdflib.URIRef('http://purl.obolibrary.org/obo/BFO_0000050')
+yes = rdflib.URIRef('http://purl.org/vodan/whocovid19crfsemdatamodel/instances/C49488')
+no = rdflib.URIRef('http://purl.org/vodan/whocovid19crfsemdatamodel/instances/C49487')
+unknown = rdflib.URIRef('http://purl.org/vodan/whocovid19crfsemdatamodel/instances/C17998')
+not_done = rdflib.URIRef('http://purl.org/vodan/whocovid19crfsemdatamodel/instances/Not_done')
+negative = rdflib.URIRef('http://purl.org/vodan/whocovid19crfsemdatamodel/instances/Negative')
+positive = rdflib.URIRef('http://purl.org/vodan/whocovid19crfsemdatamodel/instances/Positive')
+unit_label = rdflib.URIRef('http://purl.obolibrary.org/obo/IAO_0000039')
 site_id = 'EMPTY'
 
+# input
 csv_path = '../form/'
 variable_path = '../vars/'
 
+# semantic model
 onto.load('https://github.com/FAIRDataTeam/WHO-COVID-CRF/raw/master/WHO_COVID-19_Rapid_Version_CRF_Ontology.owl')
 
+# functions
 def YN2B(input):
     if input == 'Yes':
-        return Literal('True', datatype=XSD.boolean)
+        return rdflib.Literal('True', datatype=rdflib.XSD.boolean)
     elif input == 'No':
-        return Literal('False', datatype=XSD.boolean)
+        return rdflib.Literal('False', datatype=rdflib.XSD.boolean)
     else:
-        return Literal('invalid input')
+        return rdflib.Literal('invalid input')
 
 def YNU2subject(input):
     if input == 'Yes':
@@ -52,7 +58,6 @@ def YNU2subject(input):
     elif input == 'Unknown':
         return unknown
 
-
 def PNN2subject(input):
     if input == 'Positive' or input == 'Postive': #Option 'Postive' due to error in form output
         return positive
@@ -61,16 +66,15 @@ def PNN2subject(input):
     elif input == 'Not Done':
         return not_done
 
-
 def getPartOfClass(owl_class):
-    result=[]
-    superclasses = onto.transitive_objects(owl_class, RDFS.subClassOf)
+    result = []
+    superclasses = onto.transitive_objects(owl_class, rdflib.RDFS.subClassOf)
     #superclasses = onto.objects(owl_class, RDFS.subClassOf)
     for superclass in superclasses:
-        if (superclass, RDF.type, OWL.Restriction) in onto:
-            if (superclass, OWL.onProperty, part_of) in onto:
+        if (superclass, rdflib.RDF.type, rdflib.OWL.Restriction) in onto:
+            if (superclass, rdflib.OWL.onProperty, part_of) in onto:
                 tussen = superclass
-                for s, p, o in onto.triples((tussen, OWL.someValuesFrom, None)):
+                for s, p, o in onto.triples((tussen, rdflib.OWL.someValuesFrom, None)):
                     result.append(o)
     if len(result) == 0:
         print('no part of found for ' + owl_class.toPython())
@@ -80,27 +84,25 @@ def getPartOfClass(owl_class):
     else:
         return('more than 1 result')
 
-
 def getUnit(question):
-    superclasses = onto.transitive_objects(question, RDFS.subClassOf)
+    superclasses = onto.transitive_objects(question, rdflib.RDFS.subClassOf)
     for superclass in superclasses:
-        if (superclass, RDF.type, OWL.Restriction) in onto:
-            if (superclass, OWL.onProperty, unit_label) in onto:
-                for s,p,o in onto.triples((superclass, OWL.hasValue, None)):
+        if (superclass, rdflib.RDF.type, rdflib.OWL.Restriction) in onto:
+            if (superclass, rdflib.OWL.onProperty, unit_label) in onto:
+                for s,p,o in onto.triples((superclass, rdflib.OWL.hasValue, None)):
                     return(o)
 
-
 def hasLOV(list_question):
-    superclasses = onto.transitive_objects(list_question, RDFS.subClassOf)
+    superclasses = onto.transitive_objects(list_question, rdflib.RDFS.subClassOf)
     for superclass in superclasses:
-        if isinstance(superclass, BNode):
-            if next(onto.objects(superclass, OWL.onProperty)) == n.has_value:
+        if isinstance(superclass, rdflib.BNode):
+            if next(onto.objects(superclass, rdflib.OWL.onProperty)) == n.has_value:
                 #print(superclass)
-                superpart = onto.objects(superclass, OWL.onClass)
+                superpart = onto.objects(superclass, rdflib.OWL.onClass)
                 try:
                     lov = next(superpart)
                 except StopIteration:
-                        superpart = onto.objects(superclass, OWL.someValuesFrom)
+                        superpart = onto.objects(superclass, rdflib.OWL.someValuesFrom)
                         try:
                             lov2 = next(superpart)
                         except StopIteration:
@@ -108,19 +110,19 @@ def hasLOV(list_question):
                         return (lov2)
                 return(lov)
 
-
+# script
 #translate the variables in the form to the correct URI's, using config files
-var_files = listdir(variable_path)
+var_files = os.listdir(variable_path)
 for file in var_files:
     if file[-4:] == '.csv':
         with open(variable_path + file) as translate_file:
             formreader = csv.reader(translate_file, delimiter=',')
             for row in formreader:
                 if row[1] != '':
-                    var2class[row[0]] = URIRef('http://purl.org/vodan/whocovid19crfsemdatamodel/' + row[1])
+                    var2class[row[0]] = rdflib.URIRef('http://purl.org/vodan/whocovid19crfsemdatamodel/' + row[1])
 
 #Create the list of files to process, in the right order
-files = listdir(csv_path)
+files = os.listdir(csv_path)
 for file in files:
     if file[:4] == 'New_':
         readfiles.append(file)
@@ -157,9 +159,9 @@ for csv_file in readfiles:
                 if header[1] == 'Institute Abbreviation':
                     site_id = row[1]
                 participant_id = row[0]
-                form = URIRef('http://data.who.covid-19-rapid-crf/' + site_id + '-' + participant_id)
-                if (form, RDF.type, URIRef('http://purl.org/vodan/whocovid19crfsemdatamodel/who-covid-19-rapid-crf')) not in g:
-                    g.add((form, RDF.type, URIRef('http://purl.org/vodan/whocovid19crfsemdatamodel/who-covid-19-rapid-crf')))
+                form = rdflib.URIRef('http://data.who.covid-19-rapid-crf/' + site_id + '-' + participant_id)
+                if (form, rdflib.RDF.type, rdflib.URIRef('http://purl.org/vodan/whocovid19crfsemdatamodel/who-covid-19-rapid-crf')) not in g:
+                    g.add((form, rdflib.RDF.type, rdflib.URIRef('http://purl.org/vodan/whocovid19crfsemdatamodel/who-covid-19-rapid-crf')))
                     print('new form ' + form.toPython())
                 group_class_old = 0
                 module_class_old = 0
@@ -168,7 +170,7 @@ for csv_file in readfiles:
                 for item in header:
                     answer = row[column]
                     if item == 'Record Creation Date' or item == 'Report Creation Date':
-                        version_info = Literal(answer)
+                        version_info = rdflib.Literal(answer)
                     elif item == 'Report Name Custom':
                         report_name = answer
                     elif item == 'Report Parent':
@@ -177,14 +179,14 @@ for csv_file in readfiles:
                     elif item in var2class and (answer != ""):
                         #Make instance for question
                         question_class = var2class[item]
-                        question = BNode()
-                        g.add((question, RDF.type, question_class))
+                        question = rdflib.BNode()
+                        g.add((question, rdflib.RDF.type, question_class))
                         #print('            question ' + question_class.toPython())
                         #Get question group, make new if needed, make sure the question is not part of another question
                         group_class = getPartOfClass(question_class)
-                        if group_class != group_class_old and n.Question_group in onto.transitive_objects(group_class, RDFS.subClassOf):
-                            question_group = BNode()
-                            g.add((question_group, RDF.type, group_class))
+                        if group_class != group_class_old and n.Question_group in onto.transitive_objects(group_class, rdflib.RDFS.subClassOf):
+                            question_group = rdflib.BNode()
+                            g.add((question_group, rdflib.RDF.type, group_class))
                             group_class_old = group_class
                             #Make new group part of Module
                             module_class = getPartOfClass(group_class)
@@ -195,25 +197,25 @@ for csv_file in readfiles:
                                     if report_parent == 'HOSPITAL /CENTRE ADMISSION': #Looking for first module of form
                                         module_class = n.Module_1
                                         for pp in possible_parents:
-                                            if (pp, RDF.type, n.Module_1) in g:
+                                            if (pp, rdflib.RDF.type, n.Module_1) in g:
                                                 module = pp
                                                 #print('found1 ' + pp.toPython())
                                     elif report_parent[:22] == 'Daily Case Report Form': #Looking for a Daily form
                                         module_class = n.Module_2
                                         for pp in possible_parents:
-                                            if (pp, RDF.type, n.Module_2) in g:
-                                                if (pp, DC.description, Literal(report_parent)) in g:
+                                            if (pp, rdflib.RDF.type, n.Module_2) in g:
+                                                if (pp, DC.description, rdflib.Literal(report_parent)) in g:
                                                     module = pp
                                                     #print('found2 ' + pp.toPython())
                                     elif report_parent == 'DISCHARGE/DEATH': #Looking for discharge module of form
                                         module_class = n.Module_3
                                         for pp in possible_parents:
-                                            if (pp, RDF.type, n.Module_3) in g:
+                                            if (pp, rdflib.RDF.type, n.Module_3) in g:
                                                 module = pp
                                                 #print('found3 ' + pp.toPython())
                                 else:
                                     #Create new module
-                                    module = BNode()
+                                    module = rdflib.BNode()
                                     ## nuria
                                     #print('tototototot: ' + ' m ' + module + ' m_c ' + module_class)
                                     if type(module_class) == str:
@@ -224,51 +226,51 @@ for csv_file in readfiles:
                                         print('---------\ncheckpoint for module_class values, the original: {}, and the new converted to rdflib.term.Literal(): {}'.format(module_class_str,module_class))
                                         print('the types are for module_class old: {} and new: {}\n---------\n'.format(module_class_str.__class__, module_class.__class__))
                                     ##
-                                    g.add((module, RDF.type, module_class))
+                                    g.add((module, rdflib.RDF.type, module_class))
                                     print('    new module ' + module_class.toPython() + ' id: ' + module)
                                     g.add((module, part_of, form))
-                                    g.add((module, OWL.versionInfo, version_info))
+                                    g.add((module, rdflib.OWL.versionInfo, version_info))
                                     if report_name:
-                                        g.add((module, DC.description, Literal(report_name)))
+                                        g.add((module, DC.description, rdflib.Literal(report_name)))
                                     module_class_old = module_class
                             g.add((question_group, part_of, module))
                             print('        new question_group ' + group_class.toPython() + ' part of ' + module)
-                        elif URIRef('http://purl.org/vodan/whocovid19crfsemdatamodel/instances/C121939') in onto.objects(group_class, RDFS.subClassOf):
+                        elif rdflib.URIRef('http://purl.org/vodan/whocovid19crfsemdatamodel/instances/C121939') in onto.objects(group_class, rdflib.RDFS.subClassOf):
                             # Situation where a question is not part of a question_group, but directly part of Module
                             question_group = group_class
                         #Make question part of question-group
                         g.add((question, part_of, question_group))
-                        if group_class != group_class_old and URIRef('http://purl.org/vodan/whocovid19crfsemdatamodel/instances/C41116') in onto.transitive_objects(group_class, RDFS.subClassOf): #Not part of a group of questions, but of a question. It is therefor a sub-question
+                        if group_class != group_class_old and rdflib.URIRef('http://purl.org/vodan/whocovid19crfsemdatamodel/instances/C41116') in onto.transitive_objects(group_class, rdflib.RDFS.subClassOf): #Not part of a group of questions, but of a question. It is therefor a sub-question
                             #Find URI of parent question
                             parent_question_class = getPartOfClass(question_class)
-                            parent_question_uri = next(g.subjects(RDF.type, parent_question_class))
+                            parent_question_uri = next(g.subjects(rdflib.RDF.type, parent_question_class))
                             g.add((question, part_of, parent_question_uri))
 
-                        if n.Boolean_Question in onto.transitive_objects(question_class, RDFS.subClassOf):  # A Boolean question
+                        if n.Boolean_Question in onto.transitive_objects(question_class, rdflib.RDFS.subClassOf):  # A Boolean question
                             g.add((question, n.has_literal_value, YN2B(answer)))
-                        elif n.Date_Question in onto.transitive_objects(question_class, RDFS.subClassOf):  # A Date question
-                            g.add((question, n.has_literal_value, Literal(datetime.strptime(answer, '%d-%m-%Y').strftime('%Y-%m-%d'), datatype=XSD.date)))
-                        elif n.Number_question in onto.transitive_objects(question_class, RDFS.subClassOf):  # A Number question
+                        elif n.Date_Question in onto.transitive_objects(question_class, rdflib.RDFS.subClassOf):  # A Date question
+                            g.add((question, n.has_literal_value, rdflib.Literal(datetime.strptime(answer, '%d-%m-%Y').strftime('%Y-%m-%d'), datatype=rdflib.XSD.date)))
+                        elif n.Number_question in onto.transitive_objects(question_class, rdflib.RDFS.subClassOf):  # A Number question
                             try:
                                 int(answer)
-                                g.add((question, n.has_literal_value, Literal(answer, datatype=XSD.integer)))
+                                g.add((question, n.has_literal_value, rdflib.Literal(answer, datatype=rdflib.XSD.integer)))
                             except ValueError:
-                                g.add((question, n.has_literal_value, Literal(answer, datatype=XSD.decimal)))
+                                g.add((question, n.has_literal_value, rdflib.Literal(answer, datatype=rdflib.XSD.decimal)))
                             unit = getUnit(question_class)
                             if unit is not None:
                                 g.add((question, unit_label, unit))
                             else:
                                 print('no unit for: ' + question_class.toPython())
-                        elif n.PNNot_done_Question in onto.transitive_objects(question_class, RDFS.subClassOf):  # A Pos/Neg/Not done question
+                        elif n.PNNot_done_Question in onto.transitive_objects(question_class, rdflib.RDFS.subClassOf):  # A Pos/Neg/Not done question
                             g.add((question, n.has_value, PNN2subject(answer)))
-                        elif n.YNU_Question in onto.transitive_objects(question_class, RDFS.subClassOf):  # A Yes/No/Unknown
+                        elif n.YNU_Question in onto.transitive_objects(question_class, rdflib.RDFS.subClassOf):  # A Yes/No/Unknown
                             g.add((question, n.has_value, YNU2subject(answer)))
-                        elif n.Text_Question in onto.transitive_objects(question_class, RDFS.subClassOf):  # Free text question
-                            g.add((question, n.has_literal_value, Literal(answer)))
-                        elif n.List_Question in onto.transitive_objects(question_class, RDFS.subClassOf):  # List of values question
+                        elif n.Text_Question in onto.transitive_objects(question_class, rdflib.RDFS.subClassOf):  # Free text question
+                            g.add((question, n.has_literal_value, rdflib.Literal(answer)))
+                        elif n.List_Question in onto.transitive_objects(question_class, rdflib.RDFS.subClassOf):  # List of values question
                             #print(question_class, answer)
                             answer_list_uri = hasLOV(question_class)
-                            answer_list = onto.subjects(RDF.type, answer_list_uri)
+                            answer_list = onto.subjects(rdflib.RDF.type, answer_list_uri)
                             for list_item in answer_list:
                                 for labels in onto.preferredLabel(list_item):
                                     if answer in labels[1]:
@@ -277,4 +279,4 @@ for csv_file in readfiles:
                         #print(answer)
                     column += 1
 
-g.serialize(file_base_name + '_rdf.ttl', format='ttl')
+g.serialize(file_base_name + '_rdf2.ttl', format='ttl')
